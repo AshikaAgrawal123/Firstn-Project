40a3eb9383e52d52e342af68165ebce748d9d20f
AshikaAgrawal123 <ashikagarwal123@gmail.com>
2018-10-24 18:10:41 +0530

    First push

---
+++ aeiou.c
@@ -0,0 +1,16 @@
+void main()
+{
+	char str[30],i;
+	gets(str);
+	for(i=0;i!='\0';i++)
+	{
+		if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'&&str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+               {
+            	i=i+2;
+               }
+	else
+    	{
+    	printf("%c",str[i]);
+         }
+    }
+}
